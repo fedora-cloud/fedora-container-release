d2b6c311c554197567069753ad1664b6c80a7731
Clement Verna <cverna@tutanota.com>
2020-12-30 20:09:57 +0100

    use thread to process each architecture in parallel
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- src/prerelease/mod.rs
+++ src/prerelease/mod.rs
@@ -5,6 +5,7 @@ use askama::Template;
 use std::fs::{read_dir, remove_dir_all, remove_file, File};
 use std::io::{Write, Result};
 use std::process::Command;
+use std::thread;
 
 #[derive(Template)]
 #[template(path = "Containerfile")]
@@ -46,48 +47,55 @@ fn download_archive<'a>(url: &'a str) -> Archive<'a> {
 pub fn prepare_containerfiles(release: String) -> Result<()> {
     let rawhide = bodhi::get_rawhide_version().unwrap();
     let urls = koji::get_koji_archive_url(&release, &rawhide, false);
+    let mut children = vec![];
     for url in urls {
-        let archive = download_archive(&url);
-        println!("Decompress the archive {}", archive.filename);
-        Command::new("tar")
-            .arg(format!("--one-top-level={}", archive.arch))
-            .arg("-xf")
-            .arg(format!("{}", archive.filename))
-            .output()
-            .expect("failed to decompress the archive");
+        children.push(thread::spawn(move || {
+            let archive = download_archive(&url);
+            println!("Decompress the archive {}", archive.filename);
+            Command::new("tar")
+                .arg(format!("--one-top-level={}", archive.arch))
+                .arg("-xf")
+                .arg(archive.filename)
+                .output()
+                .expect("failed to decompress the archive");
 
-        let result_tar = format!("fedora-{}-{}.tar", archive.version, archive.arch);
-        for entry in read_dir(format!("{}", archive.arch))? {
-            let file = entry.unwrap();
-            if file.file_type().unwrap().is_dir() {
-                let hash = file.file_name().into_string().unwrap();
-                println!("Rename the rootfs : {}", result_tar);
-                Command::new("mv")
-                    .arg(format!("{}/{}/layer.tar", archive.arch, hash))
-                    .arg(format!("{}/{}", archive.arch, result_tar))
-                    .output()
-                    .expect("failed to move the layer.tar archive");
-                remove_dir_all(file.path())?;
-            } else if !file.file_name().into_string().unwrap().contains(".tar") {
-                remove_file(file.path())?;
+            let result_tar = format!("fedora-{}-{}.tar", archive.version, archive.arch);
+            for entry in read_dir(archive.arch).unwrap() {
+                let file = entry.unwrap();
+                if file.file_type().unwrap().is_dir() {
+                    let hash = file.file_name().into_string().unwrap();
+                    println!("Rename the rootfs : {}", result_tar);
+                    Command::new("mv")
+                        .arg(format!("{}/{}/layer.tar", archive.arch, hash))
+                        .arg(format!("{}/{}", archive.arch, result_tar))
+                        .output()
+                        .expect("failed to move the layer.tar archive");
+                    remove_dir_all(file.path()).unwrap();
+                } else if !file.file_name().into_string().unwrap().contains(".tar") {
+                    remove_file(file.path()).unwrap();
+                }
             }
-        }
 
-        println!("Compress the rootfs");
-        Command::new("xz")
-            .arg("--best")
-            .arg("-T")
-            .arg("0")
-            .arg(format!("{}/{}", archive.arch, result_tar))
-            .output()
-            .expect("failed to compress the rootfs");
+            println!("Compress the rootfs");
+            Command::new("xz")
+                .arg("--best")
+                .arg("-T")
+                .arg("0")
+                .arg(format!("{}/{}", archive.arch, result_tar))
+                .output()
+                .expect("failed to compress the rootfs");
 
-        let dockerfile = ContainerfileTemplate {
-            tag: &release,
-            result_tar: &format!("{}.xz", result_tar),
-        };
-        let mut buffer = File::create(format!("{}/Dockerfile", archive.arch))?;
-        buffer.write_all(dockerfile.render().unwrap().as_bytes())?;
+            let dockerfile = ContainerfileTemplate {
+                tag: &archive.version.split(".").collect::<Vec<&str>>().first().unwrap(),
+                result_tar: &format!("{}.xz", result_tar),
+            };
+            let mut buffer = File::create(format!("{}/Dockerfile", archive.arch)).unwrap();
+            buffer.write_all(dockerfile.render().unwrap().as_bytes()).unwrap();
+        }))
+    }
+
+    for child in children {
+        let _ = child.join();
     }
     Ok(())
 }
